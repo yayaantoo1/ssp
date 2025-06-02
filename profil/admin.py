05d86a01dc8ecac78655ebc4554f750b25ccfb94
eka <yayaanto149@gmail.com>
2025-06-02 17:15:40 +0700

    pert 11

--- profil/admin.py
+++ profil/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Barang
+
+
+class BarangAdmin(admin.ModelAdmin):
+    list_display = ('kdbrg','nama','stok','harga','link_gmb')
+    list_filter = ('nama',)
+    search_fields = ('nama','harga')
+
+
+admin.site.register(Barang, BarangAdmin)
+
--- profil/models.py
+++ profil/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Barang(models.Model):
+    kdbrg=models.CharField(max_length=8)
+    nama=models.CharField(max_length=75)
+    stok=models.IntegerField()
+    harga=models.BigIntegerField()
+    link_gmb=models.CharField(max_length=150, blank=True)
+
+    def __str__(self):
+        return self.nama
