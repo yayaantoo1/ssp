7b487003cc1d8c497220fa104ebf6dc0eac0cb01
eka <yayaanto149@gmail.com>
2025-06-09 17:13:58 +0700

    tgl9pert11

--- profil/admin.py
+++ profil/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Barang
+from .models import Barang,Jenis
 
 
 class BarangAdmin(admin.ModelAdmin):
-    list_display = ('kdbrg','nama','stok','harga','link_gmb')
-    list_filter = ('nama',)
-    search_fields = ('nama','harga')
+    list_display = ('kdbrg','nama','stok','harga','link_gmb','jenis_id')
+    list_filter = ('jenis_id',)
+    search_fields = ('nama','harga','jenis_id__nama')
+    list_per_page = 4
 
 
 admin.site.register(Barang, BarangAdmin)
 
+admin.site.register(Jenis)
--- profil/models.py
+++ profil/models.py
@@ -2,12 +2,24 @@ from django.db import models
 
 # Create your models here.
 
+
+class Jenis(models.Model):
+    nama = models.CharField(max_length=50)
+    deskripsi = models.TextField()
+
+    def __str__(self):
+        return self.nama
+    
+    
 class Barang(models.Model):
     kdbrg=models.CharField(max_length=8)
     nama=models.CharField(max_length=75)
     stok=models.IntegerField()
     harga=models.BigIntegerField()
     link_gmb=models.CharField(max_length=150, blank=True)
+    waktu_post = models.DateTimeField(auto_now_add=True)
+    jenis_id = models.ForeignKey(Jenis, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.nama
+    
